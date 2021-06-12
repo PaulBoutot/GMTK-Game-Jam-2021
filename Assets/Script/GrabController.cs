6f3d2d5bd995dd309b7a2f3cdfa193beeb394acc
William Wong <bill.ww17@gmail.com>
2021-06-12 16:13:36 -0400

    Throwing works(kinda)

--- Assets/Script/GrabController.cs
+++ Assets/Script/GrabController.cs
@@ -4,57 +4,63 @@ using UnityEngine;
 
 public class GrabController : MonoBehaviour
 {
+    //Variables the tutorial said to use
     public Transform grabDetect;
     public Transform boxHolder;
     public float rayDist;
-    public bool isHolding = false;
+    public bool isHolding;
+    public bool holdKey;
+    public float throwforce;
 
-
-    // Update is called once per frame
+    //run every frame
     void Update()
     {
+        //invisible grabby hand variable
         RaycastHit2D grabCheck = Physics2D.Raycast(grabDetect.position, Vector2.right * transform.localScale, rayDist);
-
-        if(grabCheck.collider != null && grabCheck.collider.tag == "Box")
+        
+        //Are within grabbing distance of box
+        if (grabCheck.collider != null && grabCheck.collider.tag == "Box")
         {
-            if(Input.GetKey(KeyCode.G))
+
+            //ye. I want grab
+            if (Input.GetKeyDown(KeyCode.G))
             {
                 grabCheck.collider.gameObject.transform.parent = boxHolder;
                 grabCheck.collider.gameObject.transform.position = boxHolder.position;
                 grabCheck.collider.gameObject.GetComponent<Rigidbody2D>().isKinematic = true;
-                print(isHolding);
-                isHolding = true;
-                print(isHolding);
-
+                isHolding = true;                
             }
+
+            //no i no near box
             else
             {
                 grabCheck.collider.gameObject.transform.parent = null;
                 grabCheck.collider.gameObject.GetComponent<Rigidbody2D>().isKinematic = false;
-                
-            }
-
-            if (isHolding)
-            {
-                print(isHolding);
-                if (Input.GetKey(KeyCode.G))
-                {
-                    grabCheck.collider.gameObject.transform.parent = null;
-                    grabCheck.collider.gameObject.GetComponent<Rigidbody2D>().isKinematic = false;
-                    isHolding = false;
-                    print(isHolding);
-                }
 
             }
+
+            
+        }
+
+        //i want to yeet
+        if (Input.GetKeyUp(KeyCode.G) && isHolding)
+        {
+            //grabCheck.collider.gameObject.transform.parent = null;
+            yeetTime();
 
         }
-        
-
-
-
-
-
-
 
     }
+
+    //YEET(not working)
+    void yeetTime()
+    {
+        RaycastHit2D grabCheck = Physics2D.Raycast(grabDetect.position, Vector2.right * transform.localScale, rayDist);
+        print("yeet");
+        //grabCheck.collider.gameObject.transform.parent = boxHolder;
+        grabCheck.collider.gameObject.transform.parent = null;
+        //grabCheck.collider.gameObject.GetComponent<Rigidbody2D>().isKinematic = false;
+        grabCheck.collider.gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(transform.localScale.x, 1) * throwforce;
+    }
+
 }
